2b60e2644f20631d7f02a0fb0506b23a633fbec9
m-nzioki <nzioki02@gmail.com>
2023-08-02 23:37:01 -0700

    Reviewing tasks 7 and 9 of 0x013

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
  * get_nodeint_at_index - gets the nth node of a linked list
@@ -9,12 +10,21 @@
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
+	size_t n;
 	unsigned int i;
 	listint_t *ptr = head;
 
-	for (i = 0; i < index; i++)
+	for (n = 0; ptr != NULL; n++)
 		ptr = ptr->next;
 
-	return (ptr ? ptr : NULL);
-}
+	if (index > (n - 1))
+		return (NULL);
 
+	ptr = head;
+
+	for (i = 0; i < index; i++)
+	{
+		ptr = ptr->next;
+	}
+	return (ptr);
+}
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,7 +10,7 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *ptr, *new_node;
+	listint_t *ptr, *new_node, *temp;
 	unsigned int i;
 
 	ptr = *head;
@@ -21,12 +21,19 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 
 	new_node->n = n;
-	for (i = 0; i < idx; i++)
+
+	if (idx == 0)
+		new_node->next = ptr;
+
+	ptr = ptr->next;
+
+	for (i = 1; i < idx; i++)
 	{
 		if (i == (idx - 1))
 		{
-			new_node->next = ptr->next;
+			temp = ptr->next;
 			ptr->next = new_node;
+			new_node->next = temp;
 			return (new_node);
 		}
 		ptr = ptr->next;
