6e540ceb9d25bff3f7bcc79b9ceef9e0bbc1445e
m-nzioki <nzioki02@gmail.com>
2023-08-03 00:03:40 -0700

    Reviewing tasks 9 and 10 of 0x13

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -17,7 +17,14 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if (*head == NULL)
 		return (-1);
 
-	for (i = 0; i < index; i++)
+	if (index == 0)
+	{
+		*head = (*head->next);
+		free(ptr);
+		return (1);
+	}
+
+	for (i = 0; i < (index - 1); i++)
 	{
 		if (ptr == NULL || ptr->next == NULL)
 			return (-1);
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,30 +10,32 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *ptr, *new_node, *temp;
+	listint_t *ptr, *new_node;
 	unsigned int i;
 
 	ptr = *head;
 
 	new_node = malloc(sizeof(listint_t));
 
-	if (ptr == NULL || new_node == NULL)
+	if (head == NULL || new_node == NULL)
 		return (NULL);
 
 	new_node->n = n;
+	new_node->next = NULL;
 
 	if (idx == 0)
-		new_node->next = ptr;
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
 
-	ptr = ptr->next;
-
-	for (i = 1; i < idx; i++)
+	for (i = 0; i < idx && ptr != NULL; i++)
 	{
 		if (i == (idx - 1))
 		{
-			temp = ptr->next;
+			new_node->next = ptr->next;
 			ptr->next = new_node;
-			new_node->next = temp;
 			return (new_node);
 		}
 		ptr = ptr->next;
