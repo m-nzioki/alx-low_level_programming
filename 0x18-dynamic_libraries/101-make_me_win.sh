ab852d2d7ff1bbe87d44fb6a5b01d151633004bf
m-nzioki <nzioki02@gmail.com>
2023-08-21 07:10:11 -0700

    Hot fix

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -P /tmp https://github.com/m-nzioki/alx-low_level_programming/raw/master/0x18-dynamic_libraries/spoof.so
-export LD_PRELOAD=/tmp/spoof.so ./gm 9 8 10 24 75 9
+export LD_PRELOAD=/tmp/spoof.so
